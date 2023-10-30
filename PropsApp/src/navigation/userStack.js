6881d04f31429477277dab69130462f041503339
Brendan Moorehead <bmoorehe@uoguelph.ca>
2023-10-30 15:40:30 -0400

    Added icons to bottom tab navigator

--- PropsApp/src/navigation/userStack.js
+++ PropsApp/src/navigation/userStack.js
@@ -1,8 +1,9 @@
 //When user is logged in
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import Ionicons from 'react-native-vector-icons/Ionicons'
 //Screens import
 import FriendsScreen from '../screens/Friends'
 import LeaderboardScreen from '../screens/Leaderboard'
@@ -15,12 +16,37 @@ const Tab = createBottomTabNavigator();
 
 const userStack = () => {
   return (
-    <NavigationContainer>
+    <NavigationContainer
+      style={styles.nav}
+    >
       <Tab.Navigator
-          initialRouteName="Swiping"
-          screenOptions={{
-            headerShown: false,
-        }}
+        initialRouteName="Swiping"
+        screenOptions={({ route }) => ({
+          headerShown: false,
+          
+          tabBarIcon: ({focused, color, size}) => {
+            let iconName;
+            if (route.name === 'Leaderboard'){
+              iconName = focused ? 'trophy' : 'trophy-outline';
+            }
+            else if (route.name === 'Friends'){
+              iconName = focused ? 'people' : 'people-outline';
+            }
+            else if (route.name === 'Swiping'){
+              iconName = focused ? 'swap-horizontal' : 'swap-horizontal-outline';
+            }
+            else if (route.name === 'Stats'){
+              iconName = focused ? 'bar-chart' : 'bar-chart-outline';
+            }
+            else if (route.name === 'Profile'){
+              iconName = focused ? 'person' : 'person-outline';
+            }
+            return <Ionicons name={iconName} size={size} color={color} />
+          },
+          tabBarStyle: {
+            backgroundColor: '#121212',
+          }
+        })}
       >
         <Tab.Screen name="Leaderboard" component={LeaderboardScreen}/>
         <Tab.Screen name="Friends" component={FriendsScreen}/>
@@ -31,5 +57,11 @@ const userStack = () => {
     </NavigationContainer>
   )
 }
+
+const styles = StyleSheet.create({
+  nav:{
+    backgroundColor: '#121212',
+  }
+})
 
 export default userStack
