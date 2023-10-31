bb609dd4db9c1420fe1841ed22d443edaa68e17f
Brendan Moorehead <bmoorehe@uoguelph.ca>
2023-10-31 13:37:28 -0400

    Setup firebase environment

--- PropsApp/src/screens/Swiping.js
+++ PropsApp/src/screens/Swiping.js
@@ -1,7 +1,20 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import {useEffect, useState} from 'react';
 const Swiping = () => {
+  const [games, setGames] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const fetchGames = async () => {
+    try {
+      const response = await fetch('https://api.prop-odds.com/beta/games/nfl?date={today')
+    } catch(error) {
+
+    }
+  }
+
   return (
     <SafeAreaView>
       <Text>Swiping</Text>
