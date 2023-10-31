159b34950a71081c52aa96c2ed8d2d89b63989b0
Brendan Moorehead <bmoorehe@uoguelph.ca>
2023-10-31 16:34:55 -0400

    Deployed initial game getting cloud function

--- PropsApp/functions/index.js
+++ PropsApp/functions/index.js
@@ -10,41 +10,59 @@
  const functions = require('firebase-functions');
  const admin = require('firebase-admin');
  const axios = require('axios');
+ 
  admin.initializeApp();
  
- const db = admin.firestore();
- 
- exports.addMessage = functions.https.onRequest(async (req, res) => {
-     const original = req.query.text;
+ exports.getNFLGames = functions.pubsub.schedule('0 16 * * 2') // Every Tuesday at 10:00 AM
+  .timeZone('America/New_York')
+  .onRun(async () => {
      try {
-         const writeResult = await db.collection('messages').add({ original: original });
-         res.json({ result: `Message with ID: ${writeResult.id} added.` });
+         const apiKey = functions.config().prop_odds.api_key;
+         const monapiUrl = `https://api.prop-odds.com/beta/games/nfl?date=${getNextMonday()}&tz=America/New_York&api_key=${apiKey}`;
+         const thursapiUrl = `https://api.prop-odds.com/beta/games/nfl?date=${getNextThursday()}&tz=America/New_York&api_key=${apiKey}`;
+         const sunapiUrl = `https://api.prop-odds.com/beta/games/nfl?date=${getNextSunday()}&tz=America/New_York&api_key=${apiKey}`;
+ 
+         const monGames = (await axios.get(monapiUrl)).data;
+         const thursGames = (await axios.get(thursapiUrl)).data;
+         const sunGames = (await axios.get(sunapiUrl)).data;
+ 
+         await admin.firestore().collection('nflGames').doc(getNextMonday()).set(monGames);
+         await admin.firestore().collection('nflGames').doc(getNextThursday()).set(thursGames);
+         await admin.firestore().collection('nflGames').doc(getNextSunday()).set(sunGames);
+ 
+         console.log("Games added successfully.");
+         return null; // Function executed successfully
      } catch (error) {
-         console.error("Error adding message", error);
-         res.status(500).send(error);
+         console.error('Error fetching NFL games:', error);
+         return null; // Return null even if there is an error to signify function completion
      }
  });
  
- exports.makeUppercase = functions.firestore.document('/messages/{documentId}').onCreate((snapshot, context) => {
-     const original = snapshot.data().original;
-     functions.logger.log('Uppercasing', context.params.documentId, original);
-     const uppercase = original.toUpperCase();
-     return snapshot.ref.set({ uppercase }, { merge: true });
- });
+ // ... (Your date functions here)
  
- exports.getNFLGames = functions.https.onRequest(async (req, res) => {
-    try {
-        const date = '2023-11-05';
-        const apiKey = 'w2KtYORgPjQM5ZRT4SPzo4kSKH1yPrhxadgNgavs1s';
-        const apiUrl = `https://api.prop-odds.com/beta/games/nfl?date=${date}&tz=America/New_York&api_key=${apiKey}`;
 
-        const response = await axios.get(apiUrl);
-        const games = response.data;
-        await admin.firestore().collection('nflGames').doc('2023-11-05').set(games);
-        res.status(200).send("Games added successfully.");
-    } catch (error) {
-        console.error('Error fetching NFL games:', error);
-        res.status(500).send('Internal Server Error');
-    }
-});
-
+function getNextSunday(){
+    const now = new Date();
+    //Sunday is 0
+    const dayOfWeek = now.getDay();
+    const daysUntilSunday = (7 - dayOfWeek) % 7;
+    const sunday = new Date(now);
+    sunday.setDate(now.getDate() + daysUntilSunday);
+    return sunday.toISOString().split('T')[0];
+}
+function getNextMonday(){
+    const now = new Date();
+    const dayOfWeek = now.getDay();
+    const daysUntilMonday = (1 + 7 - dayOfWeek) % 7;
+    const monday = new Date(now);
+    monday.setDate(now.getDate() + daysUntilMonday);
+    return monday.toISOString().split('T')[0];
+}
+function getNextThursday(){
+    const now = new Date();
+    const dayOfWeek = now.getDay();
+    const daysUntilThursday = (4 + 7 - dayOfWeek) % 7;
+    const thursday = new Date(now);
+    thursday.setDate(now.getDate() + daysUntilThursday);
+    return thursday.toISOString().split('T')[0];
+}
