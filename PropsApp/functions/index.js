5f233d14d01e7548b65cb18144dc8723a044caa6
Brendan Moorehead <bmoorehe@uoguelph.ca>
2023-10-31 14:09:13 -0400

    Testing firestore emulation functions working

--- PropsApp/functions/index.js
+++ PropsApp/functions/index.js
@@ -7,13 +7,27 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
-
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+ const functions = require('firebase-functions');
+ const admin = require('firebase-admin');
+ admin.initializeApp();
+ 
+ const db = admin.firestore();
+ 
+ exports.addMessage = functions.https.onRequest(async (req, res) => {
+     const original = req.query.text;
+     try {
+         const writeResult = await db.collection('messages').add({ original: original });
+         res.json({ result: `Message with ID: ${writeResult.id} added.` });
+     } catch (error) {
+         console.error("Error adding message", error);
+         res.status(500).send(error);
+     }
+ });
+ 
+ exports.makeUppercase = functions.firestore.document('/messages/{documentId}').onCreate((snapshot, context) => {
+     const original = snapshot.data().original;
+     functions.logger.log('Uppercasing', context.params.documentId, original);
+     const uppercase = original.toUpperCase();
+     return snapshot.ref.set({ uppercase }, { merge: true });
+ });
+ 
